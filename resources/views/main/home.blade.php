1f78c2f0a2b48cf18ea7b143e3a84127485e3032
seanalessandro <seanalessandrolianto@gmail.com>
2021-12-16 21:11:11 +0700

    Update halaman home

---
+++ resources/views/main/home.blade.php
@@ -0,0 +1,75 @@
+@extends('main.layout')
+
+@section('content')
+
+<div class="body-gradient">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand nav-text" href="#">Navbar</a> 
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toogle Nav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a href="#" class="selected-nav-text">
+                        Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a href="#" class="nav-text">
+                        About Us
+                    </a>
+                </li>
+                <li class="nav-item active">
+                    <a href="#" class="nav-text">
+                        FAQ
+                    </a>
+                </li>
+            </ul>
+
+            <button class="btn btn-outline-success my-2 my-sm-0">Regist Now</button>
+        </div>
+    </nav>
+
+<div class="m-10">
+
+        <div class="row">
+            <div class="col-sm">
+                <div class="jumbotron">
+                    <h1 class="display-4 jumbotron-text">UBAYA ESPORT LEAGUE SEASON 2</h1>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, hic. Labore ipsa obcaecati magnam soluta quae voluptatum corrupti cupiditate aliquam explicabo in beatae corporis optio esse debitis, assumenda similique porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium ratione iusto delectus amet sapiente. Quae dolorum iusto eius consectetur nesciunt nemo! Sunt ipsum veniam, excepturi facilis quibusdam nesciunt earum.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, hic. Labore ipsa obcaecati magnam soluta quae voluptatum corrupti cupiditate aliquam explicabo in beatae corporis optio esse debitis, assumenda similique porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium ratione iusto delectus amet sapiente. Quae dolorum iusto eius consectetur nesciunt nemo! Sunt ipsum veniam, excepturi facilis quibusdam nesciunt earum.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, hic. Labore ipsa obcaecati magnam soluta quae voluptatum corrupti cupiditate aliquam explicabo in beatae corporis optio esse debitis, assumenda similique porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium ratione iusto delectus amet sapiente. Quae dolorum iusto eius consectetur nesciunt nemo! Sunt ipsum veniam, excepturi facilis quibusdam nesciunt earum.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, hic. Labore ipsa obcaecati magnam soluta quae voluptatum corrupti cupiditate aliquam explicabo in beatae corporis optio esse debitis, assumenda similique porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium ratione iusto delectus amet sapiente. Quae dolorum iusto eius consectetur nesciunt nemo! Sunt ipsum veniam, excepturi facilis quibusdam nesciunt earum.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, hic. Labore ipsa obcaecati magnam soluta quae voluptatum corrupti cupiditate aliquam explicabo in beatae corporis optio esse debitis, assumenda similique porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium ratione iusto delectus amet sapiente. Quae dolorum iusto eius consectetur nesciunt nemo! Sunt ipsum veniam, excepturi facilis quibusdam nesciunt earum.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, hic. Labore ipsa obcaecati magnam soluta quae voluptatum corrupti cupiditate aliquam explicabo in beatae corporis optio esse debitis, assumenda similique porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium ratione iusto delectus amet sapiente. Quae dolorum iusto eius consectetur nesciunt nemo! Sunt ipsum veniam, excepturi facilis quibusdam nesciunt earum.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, hic. Labore ipsa obcaecati magnam soluta quae voluptatum corrupti cupiditate aliquam explicabo in beatae corporis optio esse debitis, assumenda similique porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium ratione iusto delectus amet sapiente. Quae dolorum iusto eius consectetur nesciunt nemo! Sunt ipsum veniam, excepturi facilis quibusdam nesciunt earum.
+
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex, hic. Labore ipsa obcaecati magnam soluta quae voluptatum corrupti cupiditate aliquam explicabo in beatae corporis optio esse debitis, assumenda similique porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium ratione iusto delectus amet sapiente. Quae dolorum iusto eius consectetur nesciunt nemo! Sunt ipsum veniam, excepturi facilis quibusdam nesciunt earum.
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm">
+                <div class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://picsum.photos/200" alt="1" class="d-block">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://picsum.photos/200" alt="2" class="d-block">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://picsum.photos/200" alt="3" class="d-block">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@endsection
