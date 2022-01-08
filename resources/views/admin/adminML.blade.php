d6a15a561acef1e5e125b5a065ba26dd51324463
leinad05 <ongkowijoyod@gmail.com>
2022-01-08 15:47:49 +0700

    Update Tampilan Admin

--- resources/views/admin/adminML.blade.php
+++ resources/views/admin/adminML.blade.php
@@ -15,31 +15,35 @@
 </div>
 @endif
 
-<table id="table" class="display" style="width:100%">
-    <thead>
-    <tr align="center">
-        <th>No</th>
-        <th>Nama Tim</th>
-        <th>Status</th>
-        <th>List Player</th>
-        <th>Action</th>
-    </tr>
-    </thead>
-    <tbody>
-        @foreach ($tim as $t)
-        <tr align="center">
-            <td>{{ $loop->iteration }}</td>
-            <td>{{ $t->nama }}</td>
-            <td>{{ $t->status }}</td>
-            <td> <a href="{{ route('admin.showPlayerML',$t->id) }}">Show</a> </td>
-            <td> 
-                <a href="{{ url('admin/acceptTimML/' . $t->id) }}" class="btn btn-info">Accept</a>
-                <a href="{{ url('admin/rejectTimML/' . $t->id) }}" class="btn btn-danger">Reject</a> 
-            </td>
+<h1>Tim Mobile Legend</h1><br>
+
+<div class="table-responsive">
+    <table id="table" class="display" style="width:100%">
+        <thead>
+        <tr align="center" style="color: white">
+            <th>No</th>
+            <th>Nama Tim</th>
+            <th>Status</th>
+            <th>List Player</th>
+            <th>Action</th>
         </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($tim as $t)
+            <tr align="center">
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $t->nama }}</td>
+                <td>{{ $t->status }}</td>
+                <td> <a href="{{ route('admin.showPlayerML',$t->id) }}">Show</a> </td>
+                <td> 
+                    <a href="{{ url('admin/acceptTimML/' . $t->id) }}" class="btn btn-info">Accept</a>
+                    <a href="{{ url('admin/rejectTimML/' . $t->id) }}" class="btn btn-danger">Reject</a> 
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
 
 @section('scripts')
--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -14,46 +14,44 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-
+    <link rel="stylesheet" href="{{asset('css/uel.css')}}">
 </head>
 
-<body>
+<body style="background-color: #222222">
+    <div class="body-gradient">
 
-    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand nav-text" href="#">
+                <img src="{{asset('images/logo-uel.png')}}" width="100" height="100" alt="Logo UEL">
+            </a> 
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/admin') }}">
-                    {{-- {{ config('app.name', 'Laravel') }} --}} Admin
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                </ul>
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        @endif
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre style="font-size: 44px; color: white;">
                                     {{ Auth::user()->name }}
                                 </a>
 
@@ -65,7 +63,7 @@
                                     {{-- @endcan --}}
 
                                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
+                                        document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -75,22 +73,33 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest
-                    </ul>
-                </div>
+                    @endguest
+                </ul>
             </div>
         </nav>
-        
-    </div>
 
-    <div class="container page">
         @yield('content')
+
+        @yield('scripts')
     </div>
-
-
-    @yield('scripts')
-
+    <div class="body-bottom">
+        <div class="row">
+            <div class="col-sm-2">
+                <img src="{{asset('images/logo-uel.png')}}" width="130" height="130" alt="Logo UEL">
+            </div>
+            <div class="col-sm-6">
+                <p class="heading-white-text">UBAYA E-Sport League Season 2</p>
+                <p class="subheading-white-text">Report Bugs and Problem: (email SI UEL disini)</p>
+                <p class="subheading-white-text">Developed by Information System UEL 2022</p>
+            </div>
+            <div class="col-sm-4">
+                <h1>LOGO IG DAN YT</h1>
+            </div>
+        </div>
+    </div>
+    
 </body>
+
 <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
 
 </html>
